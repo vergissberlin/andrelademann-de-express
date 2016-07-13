94e9cb46ee8f6f3c9421ace689a6c0f05a0d973f
André Lademann <andre.lademann@netresearch.de>
2016-07-13 15:50:36 +0200

    Initial commit

---
+++ app/models/article.js
@@ -0,0 +1,31 @@
+// Example model
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+  title: String,
+  url: String,
+  text: String
+});
+
+ArticleSchema.virtual('date')
+  .get(function () {
+    return this._id.getTimestamp();
+  });
+
+mongoose.model('Article', ArticleSchema);
+
+/*
+ var Article = mongoose.model('Article', ArticleSchema);
+
+ Article.find(function (err, posts) {
+ if( posts.length) return;
+
+ new Article({
+ title: 'Initial',
+ url:'http://example.com',
+ text: 'Lorem ipsum sit amet'
+ }).save();
+ });
+ */
---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+  rootPath = path.normalize(__dirname + '/..'),
+  env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'mongamvchandlebars'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://192.168.99.100:32774/mongamvchandlebars-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'mongamvchandlebars'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/mongamvchandlebars-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'mongamvchandlebars'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/mongamvchandlebars-production'
+  }
+};
+
+module.exports = config[env];
