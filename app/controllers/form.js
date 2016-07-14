18d4ab99d7bd32a33a38992ab55e431a8b4a0040
André Lademann <andre.lademann@netresearch.de>
2016-07-14 13:16:11 +0200

    Add syntax highlighting to admin view

---
+++ app/controllers/form.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+  router = express.Router();
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.get('/form', function (req, res) {
+
+  res.render('form', {
+    title: 'Upload formular'
+  });
+});
