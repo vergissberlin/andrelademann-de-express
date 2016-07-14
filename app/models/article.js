a6e3cc7a0369dbc56142c561310b759b3a7baa44
André Lademann <vergissberlin@googlemail.com>
2016-07-14 22:18:30 +0200

    Extend article model

--- app/models/article.js
+++ app/models/article.js
@@ -1,10 +1,17 @@
-// Article model
+/**
+ * Article Model
+ * =============
+ */
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema({
   title: String,
+    state: String,
+    datePublished: String,
+    image: String,
   url: String,
+    teaser: String,
   text: String
 });
 
@@ -27,4 +34,30 @@ mongoose.model('Article', ArticleSchema);
  text: 'Lorem ipsum sit amet'
  }).save();
  });
+
+
+ var Post = new keystone.List('Post', {
+ map: { name: 'title' },
+ autokey: { path: 'slug', from: 'title', unique: true },
+ });
+
+ Post.add({
+ title: { type: String, required: true },
+ state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
+ author: { type: Types.Relationship, ref: 'User', index: true },
+ publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
+ image: { type: Types.CloudinaryImage },
+ content: {
+ brief: { type: Types.Html, wysiwyg: true, height: 150 },
+ extended: { type: Types.Html, wysiwyg: true, height: 400 },
+ },
+ categories: { type: Types.Relationship, ref: 'PostCategory', many: true },
+ });
+
+ Post.schema.virtual('content.full').get(function () {
+ return this.content.extended || this.content.brief;
+ });
+
+ Post.defaultColumns = 'title, state|20%, author|20%, publishedDate|20%';
+ Post.register();
  */
