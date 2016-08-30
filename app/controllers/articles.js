f7ed0f4b2fb21b2aa99ed79beab760bc47518b69
André Lademann <andre.lademann@netresearch.de>
2016-08-30 18:14:29 +0200

    Edit articles

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -1,7 +1,7 @@
-var express  = require('express'),
-		router   = express.Router(),
-		mongoose = require('mongoose'),
-		Article  = mongoose.model('Article');
+var express = require('express'),
+	router = express.Router(),
+	mongoose = require('mongoose'),
+	Article = mongoose.model('Article');
 
 module.exports = function (app) {
 	app.use('/', router);
@@ -18,14 +18,81 @@ router.get('/articles', function (req, res, next) {
 				return next(err);
 			}
 			res.render('sections/article/list', {
-				title:      'Articles',
-				articles:   articles,
+				title: 'Articles',
+				articles: articles,
 				pagination: {
-					page:      1,
+					page: 1,
 					pageCount: 10
 				}
 			});
 
+		});
+});
+
+router.get('/articles/status/:status', function (req, res, next) {
+	var status = req.params.status;
+	Article.find()
+		.limit(10)
+		.where('state').equals(status)
+		.sort({updated: -1})
+		.exec(function (err, articles) {
+			if (err) {
+				return next(err);
+			}
+			res.render('sections/article/list', {
+				title: 'Articles',
+				subtitle: status.charAt(0).toUpperCase() + status.slice(1) + ' articles',
+				articles: articles,
+				status: status,
+				pagination: {
+					page: 1,
+					pageCount: 10
+				}
+			});
+		});
+});
+
+/**
+ * Add article
+ */
+router.get('/articles/add', function (req, res) {
+	res.render('sections/article/edit', {
+		title: 'Articles',
+		subtitle: 'Add articles'
+	});
+});
+
+router.post('/articles/add', function (req, res) {
+	var getSlug = require('speakingurl');
+	new Article({
+		title: req.body.title,
+		slug: getSlug(req.body.title, {lang: 'de', truncate: 80}),
+		state: req.body.state,
+		image: req.body.image,
+		teaser: req.body.teaser,
+		text: req.body.text
+	}).save();
+
+	res.redirect('/articles');
+});
+
+/**
+ * Edit article
+ */
+router.post('/articles/edit/:id', function (req, res, next) {
+	var getSlug = require('speakingurl');
+	Article.findOneAndUpdate({'_id': req.params.id},
+		{
+			title: req.body.title,
+			slug: getSlug(req.body.title, {lang: 'de', truncate: 80}),
+			state: req.body.state,
+			image: req.body.image,
+			teaser: req.body.teaser,
+			text: req.body.text
+		}, function (err, article) {
+			if (err) throw err;
+			console.log(article);
+			res.redirect('/articles');
 		});
 });
 
@@ -36,37 +103,8 @@ router.get('/articles/:slug', function (req, res, next) {
 				return next(err);
 			}
 			res.render('sections/article/detail', {
-				title:   'Articles',
-				article: article
-			});
-		});
-});
-
-
-router.get('/articles/edit/:slug', function (req, res, next) {
-	Article.findOne({'slug': req.params.slug})
-		.exec(function (err, article) {
-			if (err) {
-				return next(err);
-			}
-			res.render('sections/article/detail', {
 				title: 'Articles',
 				article: article
 			});
 		});
-});
-
-
-router.post('/articles/add', function (req, res) {
-	var getSlug = require('speakingurl');
-	new Article({
-		title:  req.body.title,
-		slug:   getSlug(req.body.title, {lang: 'de', truncate: 80}),
-		state:  req.body.state,
-		image:  req.body.image,
-		teaser: req.body.teaser,
-		text:   req.body.text
-	}).save();
-
-	res.redirect('/articles');
 });
--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ var config = {
 			name: 'andrelademannde'
 		},
 		port: process.env.PORT || 3232,
-		db: 'mongodb://192.168.99.100:32768/andrelademannde-development'
+		db: 'mongodb://192.168.99.100:32769/andrelademannde-development'
 	},
 	test:        {
 		root: rootPath,
