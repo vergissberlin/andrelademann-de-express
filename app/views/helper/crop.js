f594e419b2f3b53c8fc29382e0578979a0efc010
André Lademann <vergissberlin@googlemail.com>
2016-09-01 08:47:55 +0200

    Improve crop view helper.

--- app/views/helper/crop.js
+++ app/views/helper/crop.js
@@ -2,10 +2,13 @@
 var Handlebars = require('handlebars');
 
 Handlebars.registerHelper('crop', function (context, options) {
+	var limit = parseInt(options.hash.limit);
 	if (!context) {
 		return;
 	}
-	var limit = parseInt(options.hash.limit);
+	if (context.length < limit) {
+		return new Handlebars.SafeString(context);
+	}
 	limit = limit ? limit : 150;
-	return new Handlebars.SafeString(context.substring(0, limit) + ' …');
+	return new Handlebars.SafeString((context.match(new RegExp('.{' + limit + '}\\S*')) || [context])[0] + ' …');
 });
