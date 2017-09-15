6a4b0a373e8661d21d536d7064abd1c483328dd9
André Lademann <vergissberlin@googlemail.com>
2017-09-16 00:32:46 +0200

    Put social icons to the right

--- public/js/vendor/getsocial.js
+++ public/js/vendor/getsocial.js
@@ -1,8 +1,11 @@
 (function () {
-	var po   = document.createElement('script');
-	po.type  = 'text/javascript';
+	var po = document.createElement('script');
+	po.type = 'text/javascript';
 	po.async = true;
-	po.src   = '//api.at.getsocial.io/widget/v1/gs_async.js?id=d6c1af';
-	var s    = document.getElementsByTagName('script')[0];
+	po.src = '//api.at.getsocial.io/widget/v1/gs_async.js?id=d6c1af';
+	var s = document.getElementsByTagName('script')[0];
 	s.parentNode.insertBefore(po, s);
-})();
+}
+)();
+
+window.GSvars.inline_group_code =  "<div class=\"gs-bar gs-buttons gs-theme-right gs-right gs-flat-colors\" data-pin-any=\"true\" data-plus=\"true\" data-horizontal=\"true\" data-settings=\"native:small:round\" data-lbl=\"false\" data-lbl-txt=\"Share to\" data-networks=\"tw,gp,mail,print\" data-counter=\"true\" data-floating=\"right\" share-limit=\"100\" hide-bar=\"undefined\"><\/div>";
