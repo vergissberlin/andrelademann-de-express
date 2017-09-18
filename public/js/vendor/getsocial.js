e7bf42b8bffe6590c07db16def983fc13be3163e
André Lademann <vergissberlin@googlemail.com>
2017-09-19 00:43:27 +0200

    Kepp getsocial on the left

--- public/js/vendor/getsocial.js
+++ public/js/vendor/getsocial.js
@@ -7,5 +7,3 @@
 	s.parentNode.insertBefore(po, s);
 }
 )();
-
-window.GSvars.inline_group_code =  "<div class=\"gs-bar gs-buttons gs-theme-right gs-right gs-flat-colors\" data-pin-any=\"true\" data-plus=\"true\" data-horizontal=\"true\" data-settings=\"native:small:round\" data-lbl=\"false\" data-lbl-txt=\"Share to\" data-networks=\"tw,gp,mail,print\" data-counter=\"true\" data-floating=\"right\" share-limit=\"100\" hide-bar=\"undefined\"><\/div>";
