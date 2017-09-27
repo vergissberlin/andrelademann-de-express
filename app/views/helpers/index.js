be2ad834cee1adcfd3747fabc855174cd38ee7c8
André Lademann <vergissberlin@googlemail.com>
2017-09-27 23:20:07 +0200

    Updated drop zone

--- app/views/helpers/index.js
+++ app/views/helpers/index.js
@@ -14,6 +14,7 @@ require('./crop');
 require('./compare');
 require('./capitalize');
 require('./datetime');
+// require('./debug');
 require('./html2text');
 require('./pagination');
 require('./slug');
