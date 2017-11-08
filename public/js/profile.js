1c127bbbd1c1c412eaf0601da0a9b39a00362a13
André Lademann <vergissberlin@googlemail.com>
2017-11-08 09:23:32 +0100

    Added bluma for editing

--- public/js/profile.js
+++ public/js/profile.js
@@ -16,9 +16,36 @@
 
 $(function () {
 	var $elements = $('[data-ajax]');
-	$.each( $elements, function( index, value ) {
+	$.each($elements, function (index, value) {
 		$(this).load($(this).data().ajax);
 	});
+
+	$('.js-modal-delete, .js-modal-cancel, .js-modal-close').on('click', function () {
+		$('.modal').removeClass('is-active');
+	});
+
+	$(document).keyup(function (e) {
+		// escape key
+		if (e.keyCode == 27) {
+			$('.modal').removeClass('is-active');
+		}
+		// console.log(e.keyCode);
+	});
+	$(document).keydown(function(event) {
+			// If Control or Command key is pressed and the S key is pressed
+			// run save function. 83 is the key code for S.
+			if((event.ctrlKey || event.metaKey) && event.which == 83) {
+				// Save Function
+				event.preventDefault();
+				if (confirm('Save and exit?')) {
+					$('#submit').click();
+				}
+				return false;
+			};
+		}
+	);
+
+
 });
 
 },{}]},{},[1]);
